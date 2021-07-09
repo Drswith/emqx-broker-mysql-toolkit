425acf1380061fa13a097c980cbee87f0340574d
18607716194 <540628938@qq.com>
2021-07-09 09:20:00 +0800

    修改vuex注入方式

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@
  * @Author: Drswith
  * @Date: 2021-06-27 18:05:51
  * @LastEditors: Drswith
- * @LastEditTime: 2021-07-08 20:55:17
- * @FilePath: \emqx-broker-mysql-toolkit\src\main.js
+ * @LastEditTime: 2021-07-09 09:19:50
+ * @FilePath: \emqx-community-mysql-toolkit\src\main.js
  */
 import Vue from "vue";
 import App from "./App.vue";
+import store from "@/store";
 
 //  引入全局主题配置
 import "@/style/style.scss";
@@ -16,10 +17,7 @@ import IconFont from "./components/IconFont.vue";
 Vue.component("IconFont", IconFont);
 Vue.use(IconFont);
 
-// 注入store
-import store from "@/store";
-Vue.prototype.$store = store;
-
 new Vue({
+  store,
   render: (h) => h(App),
 }).$mount("#app");
